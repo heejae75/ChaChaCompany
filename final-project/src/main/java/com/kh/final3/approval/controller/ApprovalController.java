6e1c38b961bed383adc052b83f3112bba08a7219
tom940602 <tom940602@hanmail.net>
2023-05-30 17:37:15 +0900

    커밋_이동현

--- final-project/src/main/java/com/kh/final3/approval/controller/ApprovalController.java
+++ final-project/src/main/java/com/kh/final3/approval/controller/ApprovalController.java
@@ -1,4 +1,4 @@
-package com.kh.final3.Approval.controller;
+package com.kh.final3.approval.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
