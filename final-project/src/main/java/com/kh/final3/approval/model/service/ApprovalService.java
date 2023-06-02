2568c27b709b8a2023874147d58c03dfb838d571
tom940602 <tom940602@hanmail.net>
2023-06-02 18:16:05 +0900

    전자결재시스템

--- final-project/src/main/java/com/kh/final3/approval/model/service/ApprovalService.java
+++ final-project/src/main/java/com/kh/final3/approval/model/service/ApprovalService.java
@@ -1,8 +1,18 @@
 package com.kh.final3.approval.model.service;
 
-import org.springframework.stereotype.Service;
+import java.util.ArrayList;
 
-@Service
-public class ApprovalService {
+import com.kh.final3.approval.model.vo.ApprovalDoc;
+import com.kh.final3.common.model.vo.PageInfo;
 
+public interface ApprovalService {
+	
+	//게시글 수 조회
+	int selectListCount(String status);
+	
+	//결재함 전체조회
+	ArrayList<ApprovalDoc> selectApprovalList(PageInfo pi, String status);
+	
+	//메인 최근 목록조회
+	ArrayList<ApprovalDoc> approvalMainList(String status);
 }
